6e339a1e4495ee2d7e87a7f2acc3c2a7ebbc9175
Caesy Gibson <c.gibson@dealertrade.com.au>
2018-11-07 10:39:05 +1000

    Updated to F7 3.5.1

--- package.js
+++ package.js
@@ -1,20 +1,15 @@
 Package.describe({
-    name: 'loftsteinn:framework7-ios',
-    version: '1.6.5',
-    summary: 'Framework7 v1.6.5 for Meteor using the iOS design theme',
-    git: 'https://github.com/oskarszoon/meteor-framework7-ios',
+    name: 'CyberCyclone:framework7',
+    version: '3.5.1',
+    summary: 'Framework7 v3.5.1 for Meteor using the bundle JS',
+    git: 'https://github.com/CyberCyclone/meteor-framework7',
     documentation: 'README.md'
 });
 
 Package.onUse(function (api) {
-    api.addFiles('framework7/dist/js/framework7.js', 'client');
+    api.addFiles('framework7/packages/core/framework7.esm.bundle.js', 'client');
 
     api.addFiles([
-        'framework7/dist/css/framework7.ios.css',
-        'framework7/dist/css/framework7.ios.colors.css'
-    ], 'client');
-
-    api.addAssets([
-        'framework7/dist/img/i-f7-ios.png'
+        'framework7/packages/core/css/framework7.css',
     ], 'client');
 });
